64dceba70352ed635b176bfbcdfab19824a841ef
kritesh00 <kriteshbhattarai617@gmail.com>
2026-01-20 21:55:58 +0545

    Spinner-Loading-Testing

--- weather-app/src/Components/Card.jsx
+++ weather-app/src/Components/Card.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react';
 import Snowfall from 'react-snowfall'
+import Spinner from './Spinner';
 const Card =()=>{
     const [Description, setDescription]=useState("");
     const [Temperature, setTemparature]=useState("");
     const [text, setText]=useState("");
-   
+    const [Loading, setLoading]=useState(false);
+    
     const checkWeather=async()=>{
         
         const cityRegex = /^[A-Za-z\u00C0-\u017F]+(?:[\s\-'[A-Za-z\u00C0-\u017F]+)*$/;
@@ -12,12 +14,16 @@ const Card =()=>{
             alert("Please enter a valid city name");
             return;
         }
-        else{setDescription("Loading... please wait");}
+        else{
+            setLoading(true);
+            
+        }
         const response = await fetch(`https://goweather.xyz/weather/${text}`);
         const data = await response.json();
         setDescription("Weather: " + data.description);
        
         setTemparature("Temperature: " + data.temperature);
+        setLoading(false);
         
         setText('')
         setTimeout(() => {
@@ -35,8 +41,13 @@ const Card =()=>{
                 <h1 className='text-[#061E29] text-2xl '>Weather Checker</h1>
                 <div className=' p-4 rounded-lg w-60 ' id='content'>
                 <input type="text" name="city" id="city" placeholder="Enter city"required className='placeholder-[#5fa8c7] bg-white text-[#061E29] rounded-md text-center h-7 w-full border border-[#8CE4FF]  focus:border-[#FFA239]  focus:ring-2  focus:ring-[#FFA239]/40 outline-none transition-all duration-200' value={text} onChange={(e)=> setText(e.target.value)} /> 
+                {Loading && <Spinner />}
+                {!Loading && (
+                <>
                 <p className='text-center ' id='weather'> {Description}</p>
                 <p className='text-center ' id='temp'>{Temperature}</p>
+                </>
+                )}
                 </div>
                 
                 
