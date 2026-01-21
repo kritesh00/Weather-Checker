a48bbd21dc2d8cc5b1f55fe31a257fb745ef368a
kritesh00 <kriteshbhattarai617@gmail.com>
2026-01-21 13:53:39 +0545

    Update

--- weather-app/src/Components/WelcomeMessage.jsx
+++ weather-app/src/Components/WelcomeMessage.jsx
@@ -4,7 +4,9 @@
   return (
     <>
     <div>
-        <h1 className="text-2xl font-bold text-center mb-4 text-darkbg dark:text-[#E6F7FF]">Thank you for using the App</h1>
+        <h1 className="text-2xl font-bold text-center mb-4 text-darkbg dark:text-[#E6F7FF]">Thank you for using the App
+          🙏
+        </h1>
     </div>
     </>
   )
